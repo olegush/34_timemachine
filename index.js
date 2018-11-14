380224422a0a6ba1ddc4a0cf56c1f965b2b5ca34
olegush <43958767+olegush@users.noreply.github.com>
2018-11-14 10:08:14 +0300

    the task is ready

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var TIMEOUT_IN_SECS = 3 * 60
+var TIMEOUT_IN_SECS = 10
 var TEMPLATE = '<h1><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>'
 
 function padZero(number){
@@ -55,19 +55,16 @@ class TimerWidget{
 
     // adds HTML tag to current page
     this.timerContainer = document.createElement('div')
-
-    this.timerContainer.setAttribute("style", "height: 100px;")
+    document.body.setAttribute("style", "margin-top: 25px;")
+    this.timerContainer.setAttribute("style", "width:50px; height: 35px; padding-left: 30px; margin-top: -10px; font-size: 8px; background-color: #FFFFFF; color: #d93025; position: fixed; top: 0; left: 0; z-index: 1000;")
     this.timerContainer.innerHTML = TEMPLATE
-
     rootTag.insertBefore(this.timerContainer, rootTag.firstChild)
-
     this.minutes_element = this.timerContainer.getElementsByClassName('js-timer-minutes')[0]
     this.seconds_element = this.timerContainer.getElementsByClassName('js-timer-seconds')[0]
   }
   update(secsLeft){
     var minutes = Math.floor(secsLeft / 60);
     var seconds = secsLeft - minutes * 60;
-
     this.minutes_element.innerHTML = padZero(minutes)
     this.seconds_element.innerHTML = padZero(seconds)
   }
@@ -79,18 +76,27 @@ class TimerWidget{
   }
 }
 
-
 function main(){
-
   var timer = new Timer(TIMEOUT_IN_SECS)
   var timerWiget = new TimerWidget()
   var intervalId = null
-
   timerWiget.mount(document.body)
 
   function handleIntervalTick(){
     var secsLeft = timer.calculateSecsLeft()
     timerWiget.update(secsLeft)
+    if (secsLeft === 0) {
+      alert_messages();
+    }
+  }
+  
+  function alert_messages(){
+    var timeout_alerts_in_secs = 15;
+    var messages = ['Ну наконец-то пришла пора поработать!', 'Хабр прекрасен, но дело стоит', 'И вновь твой таймер-друг напоминает о необходимости переключиться с потребления на созидание', 'Эти нули в углу своей как бы намекают о том, что ... ', 'Уверен, что не прокрастинируешь? Или нет? В любом случае только ты знаешь, что далать.', 'Ты кнопку OK нажми, а потом "Закрыть вкладку" и пусть не отвлекает тебя ничего больше от дела', 'Хоть работа в лес не убежит, а волка-то ноги кормят.'];
+    over_secs = timer.getTimestampInSecs() - timer.timestampOnStart - timer.timeout_in_secs;
+    if (over_secs % timeout_alerts_in_secs === 0) {
+       alert(messages[Math.floor(Math.random() * messages.length)]);
+    }
   }
 
   function handleVisibilityChange(){
@@ -104,7 +110,6 @@ function main(){
     }
   }
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
   document.addEventListener("visibilitychange", handleVisibilityChange, false);
   handleVisibilityChange()
 }
@@ -112,6 +117,5 @@ function main(){
 if (document.readyState === "complete" || document.readyState === "loaded") {
   main();
 } else {
-  // initialize timer when page ready for presentation
   window.addEventListener('DOMContentLoaded', main);
 }
